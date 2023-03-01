1d1564ca1a04c761f459813efbeb09a908307271
Bruno Bahy <110056385+Brunobahy@users.noreply.github.com>
2023-03-01 17:13:59 -0300

    Update index.jsx

--- src/componentes/Calendario/index.jsx
+++ src/componentes/Calendario/index.jsx
@@ -6,7 +6,7 @@ export default function Calendario() {
         <div>
             <h2 className={styles.titulo}>Calendario de atividade</h2>
             <iframe
-                src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23E4C441&ctz=America%2FSao_Paulo&showTitle=0&showNav=0&showDate=0&showTabs=0&showPrint=0&showTz=0&showCalendars=0&src=NGJkMjc0ODkzZDYwZmMxZTI2ODlhMDZiOTk5MDRiOGE0YjUwNzVkMjlhNjQ2ZTZiMWQwOTA3MzFlOWY1NGNjZUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23D81B60"
+                src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FSao_Paulo&showTz=0&showCalendars=0&showTabs=0&showPrint=0&showDate=0&showNav=0&showTitle=0&src=NGJkMjc0ODkzZDYwZmMxZTI2ODlhMDZiOTk5MDRiOGE0YjUwNzVkMjlhNjQ2ZTZiMWQwOTA3MzFlOWY1NGNjZUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23D81B60"
                 className={styles.calendario}
             >
             </iframe>
