ebcbb9db86c55c8389de243df7f0ce658eeac7d7
Bruno Bahy <110056385+Brunobahy@users.noreply.github.com>
2023-03-06 12:04:00 -0300

    Update index.jsx

--- src/paginas/Fotos/index.jsx
+++ src/paginas/Fotos/index.jsx
@@ -32,8 +32,8 @@ export default function Fotos({lista}) {
                                 <img src={item.Foto.replace('open', 'uc')} alt="" />
                             </div>
                             <div className={styles.legenda}>
-                                <h2 className={styles.titulo}>{item.Titulo-Foto}</h2>
-                                <p className={styles.subtitulo}>{item.Info-foto}</p>
+                                <h2 className={styles.titulo}>{item.Titulo_Foto}</h2>
+                                <p className={styles.subtitulo}>{item.Info_foto}</p>
                             </div>
 
                         </li>
